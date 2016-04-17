c0485ce9ba0a669f1df9e0d2793f5b3e0a743073
Peter V. Saveliev <petr.savelyev@ericsson.com>
2016-04-17 22:15:29 +0200

    setup: debian and fedora requirements

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dnf=`which dnf 2>/dev/null`
+yum=`which yum 2>/dev/null`
+apt=`which apt-get 2>/dev/null`
+
+if [ ! -z "$apt" ]; then {
+    sudo $apt install `cat requirements-debian`
+} elif [ ! -z "$dnf" ]; then {
+    sudo $dnf install `cat requirements-fedora`
+} elif [ ! -z "$yum" ]; then {
+    sudo $yum install `cat requirements-fedora`
+} fi
+sudo pip install `cat requirements-pip`
